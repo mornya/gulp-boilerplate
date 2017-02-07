e8ce5c30debe6f5c50db54f757090b3c80a62a56
mornya <mornya@naver.com>
2017-02-07 17:11:07 +0900

    Gulp 설정 수정

--- configures/gulp/tasks/clean.js
+++ configures/gulp/tasks/clean.js
@@ -4,7 +4,7 @@ import props from '../gulp-props.config';
 
 gulp.task('clean', () => {
 	return del.sync([
-		// no clean caches
+		props.CACHE,
 		props.DEST.root,
 		`${props.SRC.CSS}/__SPRITES__`
 	]);
